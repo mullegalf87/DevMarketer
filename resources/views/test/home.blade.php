39910bb24dc3f5bd59f68ace0687af301002f45f
mullegalf87 <dario.rosciglione87@gmail.com>
2022-03-08 14:35:49 +0100

    edit graphics navbar home

--- resources/views/test/home.blade.php
+++ resources/views/test/home.blade.php
@@ -18,14 +18,13 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav ml-auto">
                     @if( auth()->guard('users_test')->check() )
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ciao {{ auth()->guard('users_test')->user()->name }}
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" onclick="change_vis('action')">Action</a>
                             <a class="dropdown-item" onclick="logout_test()">Logout</a>
                         </div>
@@ -36,10 +35,6 @@
                     </li>
                     @endif 
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
         <div id="login" class="page" style="display: none;">
@@ -160,7 +155,7 @@
                 });
                 
             }
-            
+
         </script>
     </body>
 </html>
