81979155e5c94b37f2517505fba2594df9857b0c
mullegalf87 <dario.rosciglione87@gmail.com>
2022-03-29 09:37:33 +0200

    edit ileniadesign

--- resources/views/ileniadesign/desktop/login.blade.php
+++ resources/views/ileniadesign/desktop/login.blade.php
@@ -8,7 +8,7 @@
     <div class="d-flex flex-nowrap h-100">
         <div class="col-md-12">
             <div style="">
-                <h2 class="text-center" style="font-family: 'Futura PT', sans-serif;font-size: 15px!important;padding-top:10%">ACCOUNT</h2>
+                <h2 class="text-center" style="font-family: 'Futura PT', sans-serif;font-size: 15px!important;padding-top:10%">ACCOUNTS</h2>
                 <img class="img-corner m-auto circle" style="border-radius: 50%!important;width:20%!important;" src="ileniadesign_repo/1.jpeg">
             </div>
         </div>
