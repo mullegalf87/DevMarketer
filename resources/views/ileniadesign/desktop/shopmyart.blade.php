70129249a151e2704cc252e7378d27966f4e7803
mullegalf87 <dario.rosciglione87@gmail.com>
2022-04-05 10:04:21 +0200

    edit shopmyart

--- resources/views/ileniadesign/desktop/shopmyart.blade.php
+++ resources/views/ileniadesign/desktop/shopmyart.blade.php
@@ -7,12 +7,12 @@
             <h4 class="text-center" style="font-family: 'Futura PT', sans-serif;font-size: 15px!important;">SEARCH & FILTER</h4>
         </div>
         <div class="d-flex flex-nowrap w-100">
-            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            <h4 class="text-center flex-grow-1 cat subcat_all" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
             align-items: center;justify-content: center;">ALL</h4>
-            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            <h4 class="text-center flex-grow-1 cat subcat_print" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
             align-items: center;justify-content: center;" onclick="change_vis('shopmyart_0')">STAMPE</h4>
-            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-            align-items: center;justify-content: center;" onclick="change_vis('shopmyart_1')">CARTOLINE</h4>
+            <h4 class="text-center flex-grow-1 cat subcat_postcard" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;">CARTOLINE</h4>
             <div class="text-center flex-grow-1">
                 <button class="btn btn-primary text-center w-100" style="color:#CDB4B4!important;border-color: #CDB4B4!important;">SEARCH  </button>
             </div>
@@ -24,19 +24,21 @@
             align-items: center;justify-content: center;">FILTERS</h4>
         </div>
     </div>
-    <div class="d-flex flex-nowrap w-100 mt-3" style="height: 60px;">
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Amore</h4>
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Self-love</h4>
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Compleanno</h4>
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Amicizia</h4>
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Motivazione</h4>
-        <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
-        align-items: center;justify-content: center;">Gratitudine</h4>
+    <div id="subcat_postcard" class="subcat" style="display: none;">
+        <div class="d-flex flex-nowrap w-100 mt-3" style="height: 60px;">
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(1)">Amore</h4>
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(2)">Self-love</h4>
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(3)">Compleanno</h4>
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(4)">Amicizia</h4>
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(5)">Motivazione</h4>
+            <h4 class="text-center flex-grow-1" style="font-family: 'Futura PT', sans-serif;font-size: 13px!important;display: flex;
+            align-items: center;justify-content: center;" onclick="filter_subcategory(6)">Gratitudine</h4>
+        </div>
     </div>
     <div class="d-flex flex-wrap" style="padding: 10!important;" id="append_image_shopmyart">
     </div>
@@ -96,6 +98,27 @@
             });
     
     });
+    
+}
+
+$(".cat").click("slow",function(){
+
+    $(".cat").css("text-decoration", "none");
+    $(".subcat").css("display", "none");
+
+    $('#'+$(this)[0].classList[3]).show();
+
+    $(this).css({"text-underline-offset": "5px","text-decoration": "underline"});
+
+});
+
+function filter_subcategory(num){
+    
+    $("#show_hide_append_image").removeClass("d-none");
+    
+    $(".utility_subcat").hide();
+    
+    $(".subcategory_"+num).show();
     
 }
 
