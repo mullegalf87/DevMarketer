a0a4cc03f3cd3895a203340e711818f9ff8b0b42
mullegalf87 <dario.rosciglione87@gmail.com>
2022-06-10 15:17:36 +0200

    edit test

--- resources/views/test/action.blade.php
+++ resources/views/test/action.blade.php
@@ -5,35 +5,20 @@
 <div class="p-3">
 
     <h1>Server 2</h1>
-
-    <div class="d-flex flex-nowrap">
-
-        <input class="form-control mr-3" name="token" placeholder="Paste token" id="token"/>
-
-        <button class="btn btn-primary" onclick="assign_token_api()">Assign token</button>
-
-    </div>  
-
-    <label id="success"></label>
+    <label></label>
 
 </div>
 
 <script>
 
-    function assign_token_api(){
-
-        var token=$("#token").val();
+    $("document").ready(function(data){
 
         var object={
-            token:token,
+            token:"GVeefur5x0iAeO",
         };  
 
-        add_token_api(object);
+        check_token_api(object);
 
-        // $("#success").text(add_token_api(object));
-
-    }
-
-
+    });
 
 </script>
