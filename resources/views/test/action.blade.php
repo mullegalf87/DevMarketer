a6f71e6709ae629dfbca47e9cb87e6631036e62f
mullegalf87 <dario.rosciglione87@gmail.com>
2022-06-13 11:10:27 +0200

    edit test

--- resources/views/test/action.blade.php
+++ resources/views/test/action.blade.php
@@ -5,20 +5,22 @@
 <div class="p-3">
 
     <h1>Server 2</h1>
-    <label></label>
+    <label id="success"></label>
 
 </div>
 
 <script>
 
-    $("document").ready(function(data){
-
-        var object={
+    $(function() {
+        
+        var token={
             token:"GVeefur5x0iAeO",
         };  
 
-        check_token_api(object);
-
+        var product=[{name_prod:"cell", price_prod:3.2, stock_prod:13},{name_prod:"pc", price_prod:160, stock_prod:3}];
+        
+        check_token_api(token, product);
+        
     });
 
 </script>
