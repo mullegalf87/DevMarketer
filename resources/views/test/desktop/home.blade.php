efa10b2c8cd68e26538d6bb07d58209c06c9861b
mullegalf87 <100868082+mullegalf87@users.noreply.github.com>
2022-12-23 15:37:29 +0100

    async/await

--- resources/views/test/desktop/home.blade.php
+++ resources/views/test/desktop/home.blade.php
@@ -7,6 +7,36 @@
 </div>
 
 <script>
+    async function First(){
+        console.log('Firts Func started');
+        await Second();
+        console.log('End');
+    }
+    function Second(){
+        return new Promise(async (resolve,reject)=>{
+            console.log('At Second Function');
+            await Third();
+            console.log('Second func end');
+            resolve('Data From Second function');
+        });
+    }
+    function Third(){
+        return new Promise(async (resolve,reject)=>{
+            console.log('At Third Function');
+            await Fourth();
+            console.log('Thidr func end');
+            resolve('Data From Third function');
+        });
+    }
+    function Fourth(){
+        return new Promise((resolve,reject)=>{
+            console.log('At Fourth Function');
+            console.log('Fourth func end');
+            resolve('Data From Fourth function');
+        });
+    }
+    First();
+
     function get_data(){
         $.get("get_data",{},
             function(data){
